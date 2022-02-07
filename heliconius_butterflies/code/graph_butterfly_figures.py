fc0ccb7e59c8ae0dad121ff496edf1b20ed0778e
LeslyLopezFang <llopez84@ucmerced.edu>
2022-02-07 12:04:54 -0800

    update figure 7

--- heliconius_butterflies/code/graph_butterfly_figures.py
+++ heliconius_butterflies/code/graph_butterfly_figures.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import argparse
+import logging
 
 ##Parameters
 parser = argparse.ArgumentParser()
@@ -25,63 +26,58 @@ df = df[~((df.scaffold==hmb_scaffold)&((df.start >= hmb_start)&(df.stop <= hmb_s
         & ~((df.scaffold==hmyb_scaffold)&((df.start >= hmyb_start)&(df.stop <= hmyb_stop)))]
 
 #Axes specifications
-font = {'family': 'Arial', 'color':  'black','weight': 'normal','size': 14}
+font = {'family': 'Arial', 'color':  'black','weight': 'normal','size': 12}
+dpi_size=550
 x_title='Nucleotide Diversity {}'.format(r"$(\pi)$")
 
-#D figure
-stat="D"
-d_outfile="{}/D_vs_nucleotide_diversity_butterflies".format(args.outfile)
-fig, ax = plt.subplots(figsize = ( 10 , 5 ))
+logging.getLogger('matplotlib.font_manager').disabled = True
 
-sns.scatterplot(x="pop2_pi",y=stat,data=df,alpha=0.05,color="black",s=50)
+###Figure 7
+figure_seven_outfile="{}fig7".format(args.outfile)
+fig, ax = plt.subplots(nrows=2,ncols=2,figsize = ( 15,12),sharey=False)
+
+#D
+stat="D";axes_index_first=0;axes_index_second=0
+sns.scatterplot(x="pop2_pi",y=stat,data=df,alpha=0.05,color="black",s=50,ax=ax[axes_index_first][axes_index_second])
 sns.scatterplot(x="pop2_pi",y=stat,data=hmb,alpha=1,markers='o',edgecolor='red',marker="$\circ$"
-               ,facecolors='none',s=60)
+               ,facecolors='none',s=60,ax=ax[axes_index_first][axes_index_second])
 sns.scatterplot(x="pop2_pi",y=stat,data=hmyb,alpha=1,markers='o',edgecolor='yellow',marker="$\circ$"
-               ,facecolors='none',s=60)
-ax.set_xlabel(x_title,fontdict=font)
-ax.set_ylabel(r"$D$",fontdict=font)
-plt.tight_layout()
-plt.savefig("{}.png".format(d_outfile),dpi=400)
-ax.clear()
+               ,facecolors='none',s=60,ax=ax[axes_index_first][axes_index_second])
+ax[axes_index_first][axes_index_second].set_xlabel(x_title,fontdict=font)
+ax[axes_index_first][axes_index_second].set_ylabel(r"$D$",fontdict=font)
 
-#D+ figure
-stat="D+"
-dplus_outfile="{}/Dplus_vs_nucleotide_diversity_butterflies".format(args.outfile)
-fig, ax = plt.subplots(figsize = ( 10 , 5 ))
 
-sns.scatterplot(x="pop2_pi",y=stat,data=df,alpha=0.05,color="black",s=50)
+#fD
+stat="fD";axes_index_first=0;axes_index_second=1
+sns.scatterplot(x="pop2_pi",y=stat,data=df.loc[df.D>0],alpha=0.05,color="black",s=50,ax=ax[axes_index_first][axes_index_second])
+sns.scatterplot(x="pop2_pi",y=stat,data=hmb.loc[hmb.D>0],alpha=1,markers='o',edgecolor='red',marker="$\circ$"
+               ,facecolors='none',s=60,ax=ax[axes_index_first][axes_index_second])
+sns.scatterplot(x="pop2_pi",y=stat,data=hmyb.loc[hmyb.D>0],alpha=1,markers='o',edgecolor='yellow',marker="$\circ$"
+               ,facecolors='none',s=60,ax=ax[axes_index_first][axes_index_second])
+ax[axes_index_first][axes_index_second].set_xlabel(x_title,fontdict=font)
+ax[axes_index_first][axes_index_second].set_ylabel(r"$f_D$",fontdict=font)
+
+#Df
+stat="df";axes_index_first=1;axes_index_second=0
+sns.scatterplot(x="pop2_pi",y=stat,data=df,alpha=0.05,color="black",s=50,ax=ax[axes_index_first][axes_index_second])
 sns.scatterplot(x="pop2_pi",y=stat,data=hmb,alpha=1,markers='o',edgecolor='red',marker="$\circ$"
-               ,facecolors='none',s=60)
+               ,facecolors='none',s=60,ax=ax[axes_index_first][axes_index_second])
 sns.scatterplot(x="pop2_pi",y=stat,data=hmyb,alpha=1,markers='o',edgecolor='yellow',marker="$\circ$"
-               ,facecolors='none',s=60)
-ax.set_xlabel(x_title,fontdict=font)
-ax.set_ylabel(r"$D^+$",fontdict=font)
-plt.tight_layout()
-plt.savefig("{}.png".format(dplus_outfile),dpi=400)
-ax.clear()
+               ,facecolors='none',s=60,ax=ax[axes_index_first][axes_index_second])
+ax[axes_index_first][axes_index_second].set_xlabel(x_title,fontdict=font)
+ax[axes_index_first][axes_index_second].set_ylabel(r"$d_{f}$",fontdict=font)
 
-d_dplus_outfile="{}/D-Dplus_vs_nucleotide_diversity_butterflies".format(args.outfile)
-fig, ax = plt.subplots(ncols=2,figsize = ( 15 , 5 ),sharey=False)
-stat="D"
-sns.scatterplot(x="pop2_pi",y=stat,data=df,alpha=0.05,color="black",s=50,ax=ax[0])
+#D+
+stat="D+";axes_index_first=1;axes_index_second=1
+sns.scatterplot(x="pop2_pi",y=stat,data=df,alpha=0.05,color="black",s=50,ax=ax[axes_index_first][axes_index_second])
 sns.scatterplot(x="pop2_pi",y=stat,data=hmb,alpha=1,markers='o',edgecolor='red',marker="$\circ$"
-               ,facecolors='none',s=60,ax=ax[0])
+               ,facecolors='none',s=60,ax=ax[axes_index_first][axes_index_second])
 sns.scatterplot(x="pop2_pi",y=stat,data=hmyb,alpha=1,markers='o',edgecolor='yellow',marker="$\circ$"
-               ,facecolors='none',s=60,ax=ax[0])
-ax[0].set_xlabel(x_title,fontdict=font)
-ax[0].set_ylabel(r"$D$",fontdict=font)
-
-stat="D+"
-sns.scatterplot(x="pop2_pi",y=stat,data=df,alpha=0.05,color="black",s=50,ax=ax[1])
-sns.scatterplot(x="pop2_pi",y=stat,data=hmb,alpha=1,markers='o',edgecolor='red',marker="$\circ$"
-               ,facecolors='none',s=60,ax=ax[1])
-sns.scatterplot(x="pop2_pi",y=stat,data=hmyb,alpha=1,markers='o',edgecolor='yellow',marker="$\circ$"
-               ,facecolors='none',s=60,ax=ax[1])
-ax[1].set_xlabel(x_title,fontdict=font)
-ax[1].set_ylabel(r"$D^+$",fontdict=font)
+               ,facecolors='none',s=60,ax=ax[axes_index_first][axes_index_second])
+ax[axes_index_first][axes_index_second].set_xlabel(x_title,fontdict=font)
+ax[axes_index_first][axes_index_second].set_ylabel(r"$D^+$",fontdict=font)
 
 plt.tight_layout()
-plt.savefig("{}.png".format(d_dplus_outfile),dpi=400)
+plt.savefig("{}.svg".format(figure_seven_outfile),dpi=dpi_size)
+plt.savefig("{}.png".format(figure_seven_outfile),dpi=dpi_size)
 fig.clear()
-
-
